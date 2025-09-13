d79d5dd033a628fcc763a085d9ddd7e848a6c1f3
kartik <kaxxtric001@gmail.com>
2025-09-13 22:53:39 +0530

    Added Scripts

--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,16 @@ const { Router }=require("express");
 const userRouter=Router();
 
 userRouter.post("/signup",(req,res)=>{
+    const username=req.body.username;
+    const password=req.body.password;
     res.json({
         message:"SignupEndPoint"
     })
 });
 
 userRouter.post("/signin",(req,res)=>{
+    const userName=req.body.userName;
+    const password=req.body.password;
     res.json({
         message:"SignupEndPoint"
     })
